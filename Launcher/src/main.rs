7a08e67c26474932a0a3e8bb6f4fc5ef2467d732
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-05-19 13:14:17 +0200

    Hide terminal

--- Launcher/src/main.rs
+++ Launcher/src/main.rs
@@ -1,6 +1,8 @@
+#![windows_subsystem = "windows"]
 use std::process::Command;
+
 fn main() {
-    let status = Command::new("LemnisGate/Binaries/Win64/LemnisGate-Win64-Shipping.exe").status().expect("failed to execute process");
-    println!("process finished with: {status}");
-    assert!(status.success());
+    Command::new("LemnisGate/Binaries/Win64/LemnisGate-Win64-Shipping.exe")
+        .spawn()
+        .expect("failed to execute process");
 }
